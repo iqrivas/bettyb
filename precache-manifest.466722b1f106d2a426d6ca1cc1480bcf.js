d2c6ac262f7d43b66f4c062f37e3213ace85875d
Denisse Rivas <iqrivas@gmail.com>
2020-06-10 19:51:52 -0500

    Updates

---
+++ precache-manifest.466722b1f106d2a426d6ca1cc1480bcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6825c3ff9a425a5fa8a1127c45289cb1",
+    "url": "/bettyb/index.html"
+  },
+  {
+    "revision": "58d3c53944b24aa8c0ca",
+    "url": "/bettyb/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b59846d761883b69cd9a",
+    "url": "/bettyb/static/css/main.e33737cc.chunk.css"
+  },
+  {
+    "revision": "58d3c53944b24aa8c0ca",
+    "url": "/bettyb/static/js/2.73d56d6a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/bettyb/static/js/2.73d56d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b59846d761883b69cd9a",
+    "url": "/bettyb/static/js/main.2a3125dc.chunk.js"
+  },
+  {
+    "revision": "b7fabe96526be818059a",
+    "url": "/bettyb/static/js/runtime-main.a372f7a4.js"
+  }
+]);
