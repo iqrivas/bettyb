6a6abbb44d0d2ba3d012860d7aa5507ee206983f
Denisse Rivas <iqrivas@gmail.com>
2020-06-14 23:59:26 -0500

    Update NavLinks

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -7,15 +7,15 @@ class TopNav extends React.Component {
   render() {
     return (
       <Navbar className="topnav px-5" collapseOnSelect expand="lg" variant="dark" sticky="top">
-        <Navbar.Brand href="/bettyb/"> || Betty Barreto ||</Navbar.Brand>
+        <Navbar.Brand href="/"> || Betty Barreto ||</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="/bettyb/">Inicio</Nav.Link>
-            <Nav.Link href="/bettyb/constelaciones">Constelaciones</Nav.Link>
-            <Nav.Link href="/bettyb/ejercicios">Ejercicios</Nav.Link>
-            <Nav.Link href="/bettyb/talleres">Talleres</Nav.Link>
-            <Nav.Link href="/bettyb/contacto">Contacto</Nav.Link>
+            <Nav.Link href="/">Inicio</Nav.Link>
+            <Nav.Link href="/constelaciones">Constelaciones</Nav.Link>
+            <Nav.Link href="/ejercicios">Ejercicios</Nav.Link>
+            <Nav.Link href="/talleres">Talleres</Nav.Link>
+            <Nav.Link href="/contacto">Contacto</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
